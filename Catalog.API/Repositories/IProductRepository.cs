abee2f5f1a974e71fa7bd7e78c978c314cd85199
Rafa C <rafasantcont@yahoo.com.br>
2021-07-24 20:57:12 -0300

    implementar o padrão repositório pra entidade Product

---
+++ Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProducts(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+        Task Createproduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
