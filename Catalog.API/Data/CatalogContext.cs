d9ca4d53e0843609a35e2d163ab14005ed379603
Rafa C <rafasantcont@yahoo.com.br>
2021-07-25 22:52:48 -0300

    criar docker compose com seviços: catalogdb e catalog.api

--- Catalog.API/Data/CatalogContext.cs
+++ Catalog.API/Data/CatalogContext.cs
@@ -12,10 +12,15 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var databaseName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
 
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            var collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+
+            Products = database.GetCollection<Product>(collectionName);
 
             CatalogContextSeed.SeedData(Products);
         }
